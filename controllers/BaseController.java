58474288a55dda5a78716074a7b4dc5b3d52b9d4
Margareth Mee G. de Lima <margoifma@hotmail.com>
2021-11-03 10:05:39 -0300

    creating controllers

---
+++ controllers/BaseController.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import java.util.ArrayList;
+
+public class BaseController<T>{
+    ArrayList<T> lista;
+
+    public BaseController(){
+        lista = new ArrayList<T>();
+    }
+
+    /* CRUD */ 
+    public void create(T obj){
+        lista.add(obj);
+    }
+
+    public ArrayList<T> read(){
+        return this.lista;
+    }
+
+    public void update(T obj){
+        if(this.lista.contains(obj)){
+            lista.remove(obj);
+            lista.add(obj);
+        }
+    }
+
+    void delete(T obj){
+        lista.remove(obj);
+    }
+}
---
+++ controllers/PratosController.java
@@ -0,0 +1,7 @@
+package controllers;
+
+import models.Pratos;
+
+public class PratosController extends BaseController<Pratos>{
+    
+}
